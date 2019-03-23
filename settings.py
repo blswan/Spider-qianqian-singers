8cf7ea071ac26b2d9cf73f48de62528c774f6c6c
Black Swan <1124072357@qq.com>
2019-03-24 00:43:06 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,10 @@
+# settings of mysql connection 
 TABLE = 'qianqian_music_singers'
 HOST = 'localhost'
 USER = 'root'
 PASSWORD = 'abc123456'
 PORT = 3306
 DATABASE ='spiders'
+
+# sleep time between each request
 SLEEP_TIME = 2
